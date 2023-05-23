abb69637354d443860ddefe2876b4ad7b2bb0a58
Inna Pedersen <INN@simcorp.com>
2023-05-23 14:38:02 +0200

    Revert "introduce an error commit"
    This reverts commit 599226bdba6824cf394411708dfd1df5e7aad975.

--- MyNewConsoleApplication/Program.cs
+++ MyNewConsoleApplication/Program.cs
@@ -10,7 +10,6 @@ namespace MyNewConsoleApplication
     {
         static void Main(string[] args) {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Feature branch 1 - development 2");
         }
 
     }

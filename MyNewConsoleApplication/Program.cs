8b267a6b3ef4bfbf6323caf39300103235a8a785
Inna Pedersen <INN@simcorp.com>
2023-05-23 15:39:33 +0200

    test

--- MyNewConsoleApplication/Program.cs
+++ MyNewConsoleApplication/Program.cs
@@ -10,10 +10,7 @@ namespace MyNewConsoleApplication
     {
         static void Main(string[] args) {
             Console.WriteLine("Hello world!");
-<<<<<<< HEAD
             Console.WriteLine("Feature branch 1 - development 1");
-=======
->>>>>>> abb6963 (Revert "introduce an error commit")
         }
 
     }

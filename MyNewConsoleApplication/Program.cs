b07e5be3c58d5e2e7a8ec2fcd5d07a3354912dd5
Inna Pedersen <INN@simcorp.com>
2023-05-23 15:45:55 +0200

    Removed space made by mistake

--- MyNewConsoleApplication/Program.cs
+++ MyNewConsoleApplication/Program.cs
@@ -9,7 +9,7 @@ namespace MyNewConsoleApplication
     class Program
     {
         static void Main(string[] args) {
-             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
         }
 
     }
